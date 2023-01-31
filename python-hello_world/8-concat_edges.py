3c82df407194e89035f511eb4aee47318588ec62
Josuejosue1999 <alustudent.com>
2023-01-31 07:34:54 -0800

    tout est ok

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\       language that combines remarkable power with very clear syntax"
-str = print(f"{str[38:66]} {str[-22:-18]} {str[0:7]}")
+str = print(str[38:66], str[-22:-18], str[0:7])
 print(str)
