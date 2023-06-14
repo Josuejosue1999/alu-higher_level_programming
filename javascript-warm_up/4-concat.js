c664a9a58620fb5334c7bf1ece0081c07f91a47b
Josuejosue1999 <alustudent.com>
2023-06-14 02:16:33 -0700

    task4

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const firstArgument = args[0];
-
-if (firstArgument !== undefined) {
-  console.log(`${firstArgument} is ${args[1] !== undefined ? args[1] : 'undefined'}`);
-} else {
-  console.log(`undefined is undefined`);
-}
+console.log(`${process.argv[2]} is ${process.argv[3]}`);
